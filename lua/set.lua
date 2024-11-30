17684defa96ad26a7c8c2ee167dec6e2441d7661
Rony Lee <rongyil33@gmail.com>
2024-11-30 14:33:01 +0800

    change tab width

--- lua/set.lua
+++ lua/set.lua
@@ -7,9 +7,9 @@ vim.opt.incsearch = true
 vim.opt.smartindent = true
 vim.opt.wrap = true
 -- Set <tab> space wide
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 -- Enable to switch buffer without saving
 vim.opt.hidden = true
