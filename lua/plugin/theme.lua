298092816f7f57c379d9ad1363c1abe38412977c
Rony Lee <rongyil33@gmail.com>
2025-03-12 14:27:07 +0800

    change default theme to nord

--- lua/plugin/theme.lua
+++ lua/plugin/theme.lua
@@ -1,4 +1,6 @@
 -- Theme
-vim.cmd[[set background=dark]]
-vim.cmd[[colorscheme iceberg]]
-vim.g.airline_theme="iceberg"
+vim.cmd [[set background=dark]]
+vim.cmd [[colorscheme iceberg]]
+vim.g.airline_theme = "iceberg"
+vim.cmd [[colorscheme nord]]
+vim.g.airline_theme = "nord"
