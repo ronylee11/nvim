8f58222b4b3df1f0e26c6b45ffbf64793baa0e9e
Rony Lee <rongyil33@gmail.com>
2025-03-21 13:27:40 +0800

    add italic support

--- lua/plugin/theme.lua
+++ lua/plugin/theme.lua
@@ -1,6 +1,7 @@
 -- Theme
 vim.cmd [[set background=dark]]
-vim.cmd [[colorscheme iceberg]]
-vim.g.airline_theme = "iceberg"
 --vim.cmd [[colorscheme nord]]
 --vim.g.airline_theme = "nord"
+vim.cmd [[colorscheme iceberg]]
+vim.cmd [[hi Comment gui=italic cterm=italic term=italic]]
+vim.g.airline_theme = "iceberg"
