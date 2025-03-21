b8b9e8675fabcf5464088c329f0a6634e732f498
Rony Lee <rongyil33@gmail.com>
2025-03-21 14:04:56 +0800

    set javascript keywords and function names as italic

--- lua/plugin/theme.lua
+++ lua/plugin/theme.lua
@@ -4,4 +4,8 @@ vim.cmd [[set background=dark]]
 --vim.g.airline_theme = "nord"
 vim.cmd [[colorscheme iceberg]]
 vim.cmd [[hi Comment gui=italic cterm=italic term=italic]]
+vim.cmd [[hi Keyword gui=italic guifg=#84a0c6 cterm=italic term=italic]]
+vim.cmd [[hi TSKeyword gui=italic guifg=#84a0c6 cterm=italic term=italic]]
+vim.cmd [[hi Function gui=italic cterm=italic term=italic]]
+vim.cmd [[hi TSFunction gui=italic cterm=italic term=italic]]
 vim.g.airline_theme = "iceberg"
